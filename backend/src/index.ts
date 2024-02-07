1fbbdfd03ee8a59ef4576804d854ac9b20b0caa1
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-02-07 09:43:46 +0530

    register form created and tested

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -7,12 +7,22 @@ import authRoutes from "./routes/auth"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+
 
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with the actual origin of your frontend
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow credentials
+  optionsSuccessStatus: 204, // No content status for preflight requests
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
 
 
 app.use("/api/auth", authRoutes);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './layout/Layout'
+import Register from './pages/Register'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout><p>Home Page</p></Layout>}/>
           <Route path="/search" element={<Layout><p>Search Page</p></Layout>}/>
+          <Route path="/register" element={<Layout><Register/></Layout>}/>
           <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
       </Router>
