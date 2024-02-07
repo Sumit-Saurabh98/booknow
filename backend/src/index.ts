e83cab3ac8ae5c73323402f4895d547951d44b99
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-02-07 11:11:36 +0530

    mu booking and my hotel button added and added functionalities for the verify auth token from the cookies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
+import cookieParser from "cookie-parser"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -14,12 +15,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Replace with the actual origin of your frontend
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Allow credentials
-  optionsSuccessStatus: 204, // No content status for preflight requests
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
 };
 
+app.use(cookieParser())
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client"
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
   firstName: string;
@@ -12,6 +14,10 @@ export type RegisterFormData = {
 
 const Register = () => {
 
+  const navigate = useNavigate();
+
+  const {showToast} = useAppContext()
+
   const {
     register,
     watch,
@@ -21,10 +27,11 @@ const Register = () => {
 
   const mutation = useMutation(apiClient.register, {
     onSuccess: ()=>{
-      console.log("registration successfull")
+      showToast({message: "Registration success", type: "SUCCESS"})
+      navigate("/")
     },
     onError: (error: Error) =>{
-      console.log(error.message)
+      showToast({message: error.message, type: "ERROR"})
     }
   });
 
