2fe8cae513e9c623e9eab7b0c4e9766ddb5ce78a
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-02-07 11:13:01 +0530

    bug fixes related to sign in

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -36,6 +36,7 @@ export const AppContextProvider = ({children}: {children: React.ReactNode}) =>{
     );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAppContext = () => {
     const context = useContext(AppContext);
 
